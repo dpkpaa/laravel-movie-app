c09c978b8e151fe2303728c25d532c76227bee5b
Deepak Chaudhary <32760040+dpkpaa@users.noreply.github.com>
2020-09-08 12:02:54 +0530

    Revert "ok class added to tv index"
    This reverts commit 9f360578c73640c0857ebab0c9364543f01e99ac.

--- resources/views/tv/index.blade.php
+++ resources/views/tv/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto px-4 pt-16">
-        <div class="popular-tv ok">
+        <div class="popular-tv">
             <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Shows</h2>
             <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
                 @foreach($popularTv as $tvshow)
